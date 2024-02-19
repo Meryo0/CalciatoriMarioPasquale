70f0121edc92237a4b726fedf161afea28c287be
mario <marioponti03@gmail.com>
2024-02-19 19:01:33 +0100

    Eliminato Accesso.fxml e modificato LoginAmministratore.fxml

--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -47,7 +47,7 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/Accesso.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/LoginAmministratore.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
             stage.setScene(scene);
