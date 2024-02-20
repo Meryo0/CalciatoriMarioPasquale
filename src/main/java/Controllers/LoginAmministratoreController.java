70d4cefa3d0463937cff7326488a6e3ccaa1e48e
mario <marioponti03@gmail.com>
2024-02-20 12:19:46 +0100

    Switch scene  da Login a Visiona amministratore + controllo login ;

--- src/main/java/Controllers/LoginAmministratoreController.java
+++ src/main/java/Controllers/LoginAmministratoreController.java
@@ -1,5 +1,50 @@
 package Controllers;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginAmministratoreController {
+
+    @FXML
+    TextField emailfield;
+    @FXML
+    TextField passwordfield;
+    @FXML
+    private Label wrongcred;
+    private Button button;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToVisionaAmministratore(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/gui/VisionaAmministratore.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void login (ActionEvent event) throws IOException {
+        String email = emailfield.getText() ;
+        String pword = passwordfield.getText();
+        if (email.equals("admin") && pword.equals("admin")){
+            switchToVisionaAmministratore(event);
+        }else{
+            wrongcred.setText("utente o password non valide");
+        }
+
+    }
+
 
 }
--- src/main/java/model/Main.java
+++ src/main/java/model/Main.java
@@ -52,6 +52,7 @@ public class Main extends Application{
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.show();
+
         }catch (Exception e){
             e.printStackTrace();
         }
